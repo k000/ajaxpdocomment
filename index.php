e717b78cd8a14c1baedf9b1058bff66063e85829
k000 <39722427+k000@users.noreply.github.com>
2018-07-05 11:50:25 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <html>
 <head>
   <title>ajaxとpdoのテスト。参考：Youtube</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
